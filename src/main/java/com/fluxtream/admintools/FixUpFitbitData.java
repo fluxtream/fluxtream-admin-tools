9e48c991982151ca2db34796dc512f2adaa903a3
Candide Kemmler <ckemmler@gmail.com>
2013-05-18 11:19:50 +0200

    added custom start/end- time fixup for Fitbit

--- src/main/java/com/fluxtream/admintools/FixUpFitbitData.java
+++ src/main/java/com/fluxtream/admintools/FixUpFitbitData.java
@@ -1,6 +1,11 @@
 package com.fluxtream.admintools;
 
-import java.sql.SQLException;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeConstants;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import java.sql.*;
 
 /**
  * User: candide
@@ -9,24 +14,61 @@ import java.sql.SQLException;
  */
 class FixUpFitbitData {
 
+    public static DateTimeFormatter dateStorageFormat = DateTimeFormat.forPattern(
+            "yyyy-MM-dd");
+
     public void run() throws SQLException {
         LocalTimeStorageFixUpHelper.fixUpStartAndEndTimeUsingLocalTimeStorage("Facet_FitbitSleep");
 
         fixupFitbitWeight();
         fixupFitbitActivity();
-        fixupLoggedActivity();
-    }
-
-    private void fixupLoggedActivity() throws SQLException {
-        LocalTimeStorageFixUpHelper.fixUpStartAndEndTimeUsingLocalTimeStorage("Facet_FitbitLoggedActivity");
     }
 
     private void fixupFitbitActivity() throws SQLException {
-        LocalTimeStorageFixUpHelper.fixUpStartAndEndTimeUsingLocalTimeStorage("Facet_FitbitActivity");
+        final Connection connection = Main.getConnection();
+        final Statement statement = connection.createStatement();
+        final PreparedStatement pstmt = connection.prepareStatement("UPDATE Facet_FitbitActivity SET start=?, end=? WHERE id=?");
+        final ResultSet resultSet = statement.executeQuery("SELECT id, date FROM Facet_FitbitActivity");
+        while (resultSet.next()) {
+            long id = resultSet.getLong(1);
+            String dateStorage = resultSet.getString(2);
+            final DateTime dateTime = dateStorageFormat.withZoneUTC().parseDateTime(dateStorage);
+
+            long start = dateTime.getMillis();
+            long end = dateTime.getMillis()+ DateTimeConstants.MILLIS_PER_DAY-1;
+
+            pstmt.setLong(1, start);
+            pstmt.setLong(2, end);
+            pstmt.setLong(3, id);
+
+            pstmt.executeUpdate();
+        }
     }
 
     private void fixupFitbitWeight() throws SQLException {
-        LocalTimeStorageFixUpHelper.fixUpStartAndEndTimeUsingLocalTimeStorage("Facet_FitbitWeight");
+        final Connection connection = Main.getConnection();
+        final Statement statement = connection.createStatement();
+        final PreparedStatement pstmt = connection.prepareStatement("UPDATE Facet_FitbitWeight SET start=?, end=? WHERE id=?");
+        final ResultSet resultSet = statement.executeQuery("SELECT id, date FROM Facet_FitbitWeight");
+        while (resultSet.next()) {
+            long id = resultSet.getLong(1);
+            String dateStorage = resultSet.getString(2);
+            final DateTime dateTime = dateStorageFormat.withZoneUTC().parseDateTime(dateStorage);
+
+            long start = dateTime.getMillis() + DateTimeConstants.MILLIS_PER_DAY/2;
+            long end = dateTime.getMillis() + DateTimeConstants.MILLIS_PER_DAY/2;
+
+            pstmt.setLong(1, start);
+            pstmt.setLong(2, end);
+            pstmt.setLong(3, id);
+
+            pstmt.executeUpdate();
+        }
+    }
+
+    public static void main(final String[] args) {
+        final DateTime dateTime = dateStorageFormat.withZoneUTC().parseDateTime("2013-05-18");
+        System.out.println(dateTime.getMillis());
     }
 
 
