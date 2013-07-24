8ca8bcc3f6f3bc6e2abb337c4943eb93d9e67ad5
Candide Kemmler <ckemmler@gmail.com>
2013-07-24 16:18:54 -0400

    check the existence of attributes before insertion

--- src/main/java/com/fluxtream/admintools/StoreApiKeyAttributes.java
+++ src/main/java/com/fluxtream/admintools/StoreApiKeyAttributes.java
@@ -52,7 +52,7 @@ public class StoreApiKeyAttributes {
             String[] attributeKeys = (String[]) connectorInfo.get("apiKeyAttributeKeys");
 
 	    if(attributeKeys==null) {
-		System.out.println("No attribute keys for " +  (String) connectorInfo.get("connectorName") + 
+		System.out.println("No attribute keys for " + connectorInfo.get("connectorName") +
 				   "; skipping");
 		continue;
 	    }
@@ -79,15 +79,28 @@ public class StoreApiKeyAttributes {
         final Statement statement = connect.createStatement();
         final ResultSet eachApiKey = statement.executeQuery("SELECT id FROM ApiKey WHERE api=" + api);
         PreparedStatement pstmt = connect.prepareStatement("INSERT INTO ApiKeyAttribute (attributeKey, attributeValue, apiKey_id) VALUES (?,?,?)");
+        PreparedStatement existsPstmt = connect.prepareStatement("SELECT id FROM ApiKeyAttribute WHERE attributeKey=? AND apiKey_id=?");
         final String encryptedValue = encrypter.encrypt(attributeValue);
         while(eachApiKey.next()) {
             long apiKeyId = eachApiKey.getLong(1);
-            pstmt.setString(1, attributeKey);
-            pstmt.setString(2, encryptedValue);
-            pstmt.setLong(3, apiKeyId);
-            System.out.println(String.format("apiKeyId:%s attributeKey:%s attributeValue:%s encryptedValue:%s", apiKeyId, attributeKey, attributeValue, encryptedValue));
-            pstmt.executeUpdate();
+            if (attributeKeyNotPresent(existsPstmt, attributeKey, apiKeyId)) {
+                pstmt.setString(1, attributeKey);
+                pstmt.setString(2, encryptedValue);
+                pstmt.setLong(3, apiKeyId);
+                System.out.println(String.format("apiKeyId:%s attributeKey:%s attributeValue:%s encryptedValue:%s", apiKeyId, attributeKey, attributeValue, encryptedValue));
+                pstmt.executeUpdate();
+            }
         }
+    }
+
+    /**
+     *  WARNING: THIS IS UNTESTED
+     */
+    private boolean attributeKeyNotPresent(PreparedStatement existsPstmt, String attributeKey, long apiKeyId) throws SQLException {
+        existsPstmt.setString(1, attributeKey);
+        existsPstmt.setLong(2, apiKeyId);
+        final ResultSet resultSet = existsPstmt.executeQuery();
+        return !resultSet.next();
     }
 
     private List<Map<String, Object>> getConnectorInfos(Statement statement) throws SQLException {
